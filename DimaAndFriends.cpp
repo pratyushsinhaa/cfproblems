6ca9a25c9ac7f765aa37056ad15b29e97f1dc904
Pratyush Sinha <105473440+pratyushsinhaa@users.noreply.github.com>
2023-12-18 14:56:13 +0530

    new solution

---
+++ DimaAndFriends.cpp
@@ -0,0 +1,24 @@
+/*solution to problem 272A titled Dima and Friends*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, f, total(0), answer(0);
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> f;
+        total += f;
+    }
+    for (int i = 1; i <= 5; ++i)
+    {
+        if ((total + i) % (n + 1) != 1)
+        {
+            answer += 1;
+        }
+    }
+    cout << answer << endl;
+    return 0;
+}
